1af266b027bebbbf0cb3dcccdae3332e6a71d9d2
abdoulfataho <yamyaogofataho@gmail.com>
2024-08-27 10:24:58 -0400

    sp9

---
+++ Glab-SP9/src/main/java/com/example/Glab_SP9/GlabSp9Application.java
@@ -0,0 +1,13 @@
+package com.example.Glab_SP9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlabSp9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlabSp9Application.class, args);
+	}
+
+}
---
+++ Glab-SP9/src/test/java/com/example/Glab_SP9/GlabSp9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Glab_SP9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlabSp9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
